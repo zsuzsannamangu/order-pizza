761c645136b4b3b85b6657d3ea9d6d7e68cb9cf5
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-12 13:30:40 -0700

    working on debugging business logic for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 //Business logic
 function Pizza(size) {
-  // 14in plain is default
   this.size = size;
-  // this.toppings = [];
+  this.toppings = [];
 }
 
 // Pizza.prototype.selectSize = function(size) {
 //   this.size = size; // this.size is an attribute of the object, the object is the instance of Pizza.
 // }
 
-// Pizza.prototype.selectTopping = function(topping) { // this topping is the argument of the function "selectTopping"
-//   this.toppings.push(topping);
-// }
+Pizza.prototype.selectTopping = function(topping) {
+  this.toppings.push(topping);
+}
 
 Pizza.prototype.possibleSizes = {
   "small": 10,
@@ -19,26 +18,22 @@ Pizza.prototype.possibleSizes = {
   "large": 18
 }
 
-// Pizza.prototype.possibleToppings = {
-//   "spinach": 1,
-//   "artichoke": 2,
-//   "mushroom": 2,
-//   "tomatoes": 2,
-//   "truffle oil": 4
-// }
+Pizza.prototype.possibleToppings = {
+  "spinach": 1,
+  "artichoke": 2,
+  "mushroom": 2,
+  "tomatoes": 2,
+  "truffle oil": 4
+}
 
 Pizza.prototype.pizzaCost = function() {
   var cost = 0;
-  if (this.size in this.possibleSizes){
+  this.size in this.possibleSizes
     cost = this.possibleSizes[this.size] + cost
-  }
-  // cost = this.possibleSizes[this.size]
-  // console.log(cost)
-  // this.toppings.forEach(function(topping) {
-  // // console.log(topping, possibleToppings[topping]);
-  // cost = cost + this.possibleToppings[topping]
-  // });
-  // console.log(cost)
+
+  this.toppings.forEach(function(topping) {
+    cost = cost + this.possibleToppings[topping]
+  });
   return cost
 }
 
@@ -47,12 +42,12 @@ $(document).ready(function() {
   $(".btn1").click(function() {
     var pizza = new Pizza(
       $("#size").val(),
-      // $("#toppings").val()
+      $("#toppings").val()
     )
     var cost = pizza.pizzaCost();
 
     $(".totalcost").text(parseInt(cost));
-    $(".finalcost").show();
+    $(".finalCost").show();
 
     console.log(parseInt(cost))
     // console.log(cost.pizzaCost())
