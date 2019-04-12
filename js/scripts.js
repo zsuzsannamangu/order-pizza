e4c1bb2ccfdd048146c08f0158ec170fc4387e07
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-12 09:52:37 -0700

    add constructor Pizza in javascript business logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,20 @@ function Pizza() {
 }
 
 Pizza.prototype.selectSize = function(size) {
-  this.size = size;
+  this.size = size; // this.size is an attribute of the object, the object is the instance of Pizza.
 }
 
 Pizza.prototype.selectTopping = function(topping) { // this topping is the argument of the function "selectTopping"
   this.toppings.push(topping);
 }
 
-Pizza.prototype.possibleSizes = {
+var possibleSizes = {
   "10inch": 10,
   "14inch": 14,
   "18inch": 18
 }
 
-Pizza.prototype.possibleToppings = {
+var possibleToppings = {
   "spinach": 1,
   "artichoke": 2,
   "mushroom": 2,
@@ -26,12 +26,22 @@ Pizza.prototype.possibleToppings = {
   "truffle oil": 4
 }
 
-// Pizza.prototype.pizzaCost = function() {
-//
-// }
+Pizza.prototype.pizzaCost = function() {
+  var cost = 0;
+  this.toppings.forEach(function(topping) {
+    console.log(topping, possibleToppings[topping]);
+    cost += possibleToppings[topping]
+  });
+  return cost
+};
 
+var pizza = new Pizza()
 
-// var pizza = new Pizza()
+pizza.selectTopping('mushroom')
+pizza.selectTopping('spinach')
+console.log(pizza.pizzaCost())
+
+// var pizza = new Pizza() // "specific" pizza is the instance of "abstract" Pizza "class".
 // console.log(pizza.size)
 //
 // pizza.selectSize("18inch") // when I call the function here that in () I put in the value (18inch) of the variable (size).
@@ -39,8 +49,6 @@ Pizza.prototype.possibleToppings = {
 //
 // pizza.size = '18inchwff'
 // console.log(pizza.size)
-
-
 
 
 
